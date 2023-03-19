c1861b0bad0b51d78b63662cf5d652a592f3b049
Thore Andresen <thoreand@gmx.de>
2023-03-19 12:34:19 +0100

    Include Laks2

--- src/AnkiDeckGenerator/anki/Decks.py
+++ src/AnkiDeckGenerator/anki/Decks.py
@@ -16,6 +16,9 @@ class HomogeneousDeck:
     def add_notes(self, notes: list):
         for note in notes:
             self.add_note(note)
+    
+    def add_notes_from_data(self, data: VocabularyData):
+        self.add_notes(data.get_vocabulary_notes_data())
 
     def write_to_file(self, file_name: str):
         self.deck.write_to_file(file_name)
--- src/AnkiDeckGenerator/generator.py
+++ src/AnkiDeckGenerator/generator.py
@@ -3,19 +3,28 @@ from anki.Decks import HomogeneousDeck
 from data.pandasdata import PandasVocabularyData, GoogleSpreadSheet
 
 
-def main():
+def create_sprachkurs1_vocabulary_deck():
     source_sheet = GoogleSpreadSheet(
         sheet_id = '1WThbHIfG1n0XsOx5lN47h0GI-EluMkFzc_h72JpsWLs'
     )
-    dataframe = PandasVocabularyData(source_sheet.get_data_from('Lesebuch1Laks1'))
-    model = SymmetricVocabularyNoteModel()
+    laks1_words = PandasVocabularyData(
+        source_sheet.get_data_from('FriesischerSprachkurs1Laks1')
+    )
+    laks2_words = PandasVocabularyData(
+        source_sheet.get_data_from('FriesischerSprachkurs1Laks2')
+    )
     deck = HomogeneousDeck(
         id = 2059396110,
-        name = 'Frasche uurde',
-        model = model
+        name = 'Friesischer Sprachkurs Frasch 1',
+        model = SymmetricVocabularyNoteModel()
     )
-    deck.add_notes(dataframe.get_vocabulary_notes_data())
-    deck.write_to_file('vocabulary.apkg')
+    deck.add_notes_from_data(laks1_words)
+    deck.add_notes_from_data(laks2_words)
+    deck.write_to_file('sprachkurs1-uurde.apkg')
+
+
+def main():
+    create_sprachkurs1_vocabulary_deck()
 
 
 if __name__ == '__main__':
