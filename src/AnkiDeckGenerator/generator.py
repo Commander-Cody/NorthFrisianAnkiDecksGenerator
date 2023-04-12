8b02242650d52f38ad98c3de8db87569ed7f28ff
Thore Andresen <thoreand@gmx.de>
2023-04-12 23:25:13 +0200

    Include more sheets

--- src/AnkiDeckGenerator/generator.py
+++ src/AnkiDeckGenerator/generator.py
@@ -16,22 +16,18 @@ def create_sprachkurs1_vocabulary_deck():
         word_examples='Frasche baispale',
         translated_examples='Tjüsche baispale'
     )
-    laks1_words = PandasVocabularyData(
-        source_sheet.get_data_from('FriesischerSprachkurs1Laks1'),
-        column_headings
-    )
-    laks2_words = PandasVocabularyData(
-        source_sheet.get_data_from('FriesischerSprachkurs1Laks2'),
-        column_headings
-    )
-    
+
     deck = HomogeneousDeck(
         id = 2059396110,
         name = 'Friesischer Sprachkurs - Frasch 1',
         model = vocabulary_models.frasch_from_german()
     )
-    deck.add_notes_from_data(laks1_words)
-    deck.add_notes_from_data(laks2_words)
+    for i in range(1,4):
+        words_data = PandasVocabularyData(
+            source_sheet.get_data_from(f'FriesischerSprachkurs1Laks{i}'),
+            column_headings
+        )
+        deck.add_notes_from_data(words_data)
     deck.write_to_file('sprachkurs1-uurde.apkg')
 
 
