ff7847dcc4d1e6d1fd4591f25a015d1daacbefaa
Thore Andresen <thoreand@gmx.de>
2023-04-18 22:59:23 +0200

    Create Frasch - english vocabulary deck

--- src/AnkiDeckGenerator/generator.py
+++ src/AnkiDeckGenerator/generator.py
@@ -32,8 +32,34 @@ def create_sprachkurs1_vocabulary_deck():
     deck.write_to_file('sprachkurs1-uurde.apkg')
 
 
+def create_sprachkurs1_vocabulary_deck_english():
+    source_sheet = GoogleSpreadSheet('1WThbHIfG1n0XsOx5lN47h0GI-EluMkFzc_h72JpsWLs')
+    column_headings = SymmetricVocabularyNoteData(
+        word='Frasch uurd',
+        meaning='Änglisch ouerseeting',
+        word_alternatives='Ålternatiiwe',
+        word_examples='Frasche baispale',
+        translated_examples='Änglische baispale'
+    )
+
+    deck = HomogeneousDeck(
+        id = 2059396110,
+        name = 'Frisian language course - Frasch 1',
+        model = vocabulary_models.frasch_from_english(),
+        data_transform = lenient_processor
+    )
+    partial_data_set = PandasVocabularyData(
+        source_sheet.get_data_from(f'FriesischerSprachkurs1Laks1'),
+        column_headings
+    )
+    deck.add_notes_from_data(partial_data_set)
+    
+    deck.write_to_file('languagecourse1-uurde.apkg')
+
+
 def main():
-    create_sprachkurs1_vocabulary_deck()
+    # create_sprachkurs1_vocabulary_deck()
+    create_sprachkurs1_vocabulary_deck_english()
 
 
 if __name__ == '__main__':
