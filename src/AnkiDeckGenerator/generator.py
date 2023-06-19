dd858fa973746eaa2abc42a581646c87db508785
Thore Andresen <thoreand@gmx.de>
2023-06-19 13:12:53 +0200

    Add deck with full vocabulary list from german language course

--- src/AnkiDeckGenerator/generator.py
+++ src/AnkiDeckGenerator/generator.py
@@ -3,7 +3,7 @@ from anki.decks import HomogeneousDeck
 from data.pandasdata import PandasVocabularyData, GoogleSpreadSheet
 
 import vocabularymodels as vocabulary_models
-from vocabularydataprocessors import default_processor, lenient_processor
+import vocabularydataprocessors as data_processors
 
 
 def create_sprachkurs1_vocabulary_deck():
@@ -20,7 +20,7 @@ def create_sprachkurs1_vocabulary_deck():
         id = 2059396110,
         name = 'Friesischer Sprachkurs - Frasch 1',
         model = vocabulary_models.frasch_from_german(),
-        data_transform = default_processor
+        data_transform = data_processors.default_processor
     )
     for i in range(1,5):
         partial_data_set = PandasVocabularyData(
@@ -46,7 +46,7 @@ def create_sprachkurs1_vocabulary_deck_english():
         id = 2059396110,
         name = 'Frisian language course - Frasch 1',
         model = vocabulary_models.frasch_from_english(),
-        data_transform = lenient_processor
+        data_transform = data_processors.lenient_processor
     )
     partial_data_set = PandasVocabularyData(
         dataframe = source_sheet.get_data_from(f'FriesischerSprachkurs1Laks1'),
@@ -71,7 +71,7 @@ def create_sprachkurs1_vocabulary_deck_halifreesk():
         id = 2059396110,
         name = 'Friesischer Sprachkurs - Halifreesk 1',
         model = vocabulary_models.halifreesk_from_german(),
-        data_transform = lenient_processor
+        data_transform = data_processors.lenient_processor
     )
     partial_data_set = PandasVocabularyData(
         dataframe = source_sheet.get_data_from(f'FriesischerSprachkurs1Laks1'),
@@ -82,10 +82,37 @@ def create_sprachkurs1_vocabulary_deck_halifreesk():
     deck.write_to_file('sprachkurs1-üürde.apkg')
 
 
+def create_sprachkurs_copmlete_vocab_list_german():
+    source_sheet = GoogleSpreadSheet('1lAsUOMYPuMcLSXa0jpXXjP4wGskT9OF1Wx2HscxTEmM')
+    column_headings = SymmetricVocabularyNoteData(
+        word='Frasch uurd',
+        meaning='Tjüsch ouerseeting',
+        word_alternatives='Ålternatiiwe',
+        word_examples='Frasche baispale',
+        translated_examples='Tjüsche baispale'
+    )
+
+    deck = HomogeneousDeck(
+        id = 1659476110,
+        name = 'Friesischer Sprachkurs I/II - Alle Vokabeln',
+        model = vocabulary_models.frasch_from_german(),
+        data_transform = data_processors.default_processor
+    )
+    partial_data_set = PandasVocabularyData(
+        dataframe = source_sheet.get_data_from('Uurdelist'),
+        column_headings = column_headings
+    )
+    deck.add_notes_from_data(partial_data_set)
+    
+    deck.write_to_file('sprachkurs-vokabelliste.apkg')
+
+
+
 def main():
     create_sprachkurs1_vocabulary_deck()
     # create_sprachkurs1_vocabulary_deck_english()
     # create_sprachkurs1_vocabulary_deck_halifreesk()
+    # create_sprachkurs_copmlete_vocab_list_german()
 
 
 if __name__ == '__main__':
